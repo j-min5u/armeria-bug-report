92a87814d9754fb618cc3df72910e947c88c7324
Minsoo Cheong <54794500+j-min5u@users.noreply.github.com>
2022-07-06 16:39:47 +0900

    remove corp specific name

--- src/test/kotlin/armeria/example/Authenticate.kt
+++ src/test/kotlin/armeria/example/Authenticate.kt
@@ -6,12 +6,12 @@ import com.linecorp.armeria.server.annotation.DecoratorFactoryFunction
 import com.linecorp.armeria.server.auth.AuthService
 import java.util.function.Function
 
-@DecoratorFactory(QandaAuthDecoratorFactoryFunction::class)
+@DecoratorFactory(MyAuthDecoratorFactoryFunction::class)
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.TYPE)
 annotation class Authorize
 
-class QandaAuthDecoratorFactoryFunction : DecoratorFactoryFunction<Authorize> {
+class MyAuthDecoratorFactoryFunction : DecoratorFactoryFunction<Authorize> {
     override fun newDecorator(parameter: Authorize): Function<in HttpService, out HttpService> {
         return AuthService.newDecorator(
             MyAuthenticator()
